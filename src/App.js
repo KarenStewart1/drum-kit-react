f3a98d4db4550144703a5448da83d24bc199c524
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-21 13:43:53 +0100

    Basic set up and styling for one button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Drums from "./Drums";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="row drums">
+        <Drums />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Drums.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Drums() {
+  return (
+    <div className="Drums">
+    </div>
+  );
+}

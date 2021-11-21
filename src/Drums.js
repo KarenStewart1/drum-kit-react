b4db600fc295b91fcd8696ca761cebc01bcfe51c
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-21 14:02:34 +0100

    created all drum buttons and added sound files

--- src/Drums.js
+++ src/Drums.js
@@ -1,8 +1,28 @@
 import React from "react";
+import "./Drums.css";
 
 export default function Drums() {
+  let drumArray = [
+    { sound: "CLAP", letter: "A" },
+    { sound: "HIHAT", letter: "S" },
+    { sound: "KICK", letter: "D" },
+    { sound: "OPENHAT", letter: "F" },
+    { sound: "BOOM", letter: "G" },
+    { sound: "RIDE", letter: "H" },
+    { sound: "SNARE", letter: "J" },
+    { sound: "TOM", letter: "K" },
+    { sound: "TINK", letter: "L" },
+  ];
   return (
     <div className="Drums">
+      {drumArray.map(function (drum, index) {
+        return (
+          <button className="button" key={index}>
+            {drum.letter}
+            <div className="sound">{drum.sound}</div>
+          </button>
+        );
+      })}
     </div>
   );
 }

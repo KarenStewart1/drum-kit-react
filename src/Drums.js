fb8a0c7ff6f9f5b9de21d03e9ddafa0d7677252d
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-22 13:50:32 +0100

    capitalizing and uppercase of button values

--- src/Drums.js
+++ src/Drums.js
@@ -3,15 +3,15 @@ import "./Drums.css";
 
 export default function Drums() {
   let drumArray = [
-    { sound: "CLAP", letter: "A" },
-    { sound: "HIHAT", letter: "S" },
-    { sound: "KICK", letter: "D" },
-    { sound: "OPENHAT", letter: "F" },
-    { sound: "BOOM", letter: "G" },
-    { sound: "RIDE", letter: "H" },
-    { sound: "SNARE", letter: "J" },
-    { sound: "TOM", letter: "K" },
-    { sound: "TINK", letter: "L" },
+    { sound: "./sounds/clap.wav", word: "clap", letter: "a" },
+    { sound: "./sounds/Hihat.wav", word: "Hihat", letter: "s" },
+    { sound: "./sounds/kick.wav", word: "kick", letter: "d" },
+    { sound: "./sounds/openhat.wav", word: "openhat", letter: "f" },
+    { sound: "./sounds/boom.wav", word: "boom", letter: "g" },
+    { sound: "./sounds/ride.wav", word: "ride", letter: "h" },
+    { sound: "./sounds/snare.wav", word: "snare", letter: "i" },
+    { sound: "./sounds/tom.wav", word: "tom", letter: "j" },
+    { sound: "./sounds/tink.wav", word: "tink", letter: "l" },
   ];
 
   function handleClick(event) {

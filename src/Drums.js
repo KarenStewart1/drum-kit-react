d3ef743b4b912fb46030da6f5cf792ba6a12ce54
Karen Stewart <karenstewart1117@gmail.com>
2022-01-27 13:17:14 +0100

    update variables for importing sound files

--- src/Drums.js
+++ src/Drums.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./Drums.css";
+import clap from "./sounds/clap.wav";
+import hihat from "./sounds/hihat.wav";
+import kick from "./sounds/kick.wav";
+import openhat from "./sounds/openhat.wav";
+import boom from "./sounds/boom.wav";
+import ride from "./sounds/ride.wav";
+import snare from "./sounds/snare.wav";
+import tom from "./sounds/tom.wav";
+import tink from "./sounds/tink.wav";
 
 export default function Drums() {
   const drumArray = {
